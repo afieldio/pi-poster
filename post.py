7ff532d557a14db21d76a05bc5034d2faccae4b8
afieldio <a.field738@gmail.com>
2016-07-25 12:21:57 +0100

    added try except

--- post.py
+++ post.py
@@ -15,13 +15,16 @@ def post(sn, d):
 
     try:
         requests.post('http://crox.io/sensor/sensorsapi/',
-                         auth=HTTPBasicAuth('aquaman', 'aquaponics'), data=payload)
+                      auth=HTTPBasicAuth('aquaman', 'aquaponics'), data=payload)
     except Exception, e:
-        TEXT = "An error has occured when sending the data to the server. The Exception was this: {0}".format(e)
-        sendMail('Aquaman@Aquaponics.com', 'a.field738@gmail.com', 'Error has occured', TEXT)
+        TEXT = "An error has occured when sending the data to the server. The Exception was this: {0}".format(
+            e)
+        sendMail('Aquaman@Aquaponics.com', 'a.field738@gmail.com',
+                 'Error has occured', TEXT)
         raise e
 
-def sendMail(FROM,TO,SUBJECT,TEXT):
+
+def sendMail(FROM, TO, SUBJECT, TEXT):
     """this is some test documentation in the function"""
     message = textwrap.dedent("""\
         From: %s
@@ -42,37 +45,55 @@ def post_sensor_data():
     dallasTemps = get_dallas_temps()
 
     # Dallas Sensors
-    sump_temp = dallasTemps['sump_tank']
-    data.update({'ST': sump_temp})
+    try:
+        sump_temp = dallasTemps['sump_tank']
+        data.update({'ST': sump_temp})
+    except Exception, e:
+        pass
 
-    fish_temp = dallasTemps['fish_tank']
-    data.update({'FT': fish_temp})
+    try:
+        fish_temp = dallasTemps['fish_tank']
+        data.update({'FT': fish_temp})
+    except Exception, e:
+        pass
 
-    grow_temp = dallasTemps['grow_bed']
-    data.update({'GT': grow_temp})
+    try:
+        grow_temp = dallasTemps['grow_bed']
+        data.update({'GT': grow_temp})
+    except Exception, e:
+        pass
 
     # Air Sensor
-    air_Sensor = BMP085.BMP085()
-    
-    air_temp = air_Sensor.read_temperature()
-    data.update({'AT': air_temp})
+    try:
+        air_Sensor = BMP085.BMP085()
+        air_temp = air_Sensor.read_temperature()
+        data.update({'AT': air_temp})
+    except Exception, e:
+        pass
 
-    air_press = air_Sensor.read_pressure()
-    data.update({'AP': air_press})
+    try:
+        air_press = air_Sensor.read_pressure()
+        data.update({'AP': air_press})
+    except Exception, e:
+        TEXT = "An error has occured when sending the data to the server. The Exception was this: {0}".format(
+            e)
+        sendMail('Aquaman@Aquaponics.com', 'a.field738@gmail.com',
+                 'Error has occured', TEXT)
+        pass
 
     # Light Sensor
-	try:
-		light_sensor = TSL2561()
-		light_lux = light_sensor.readLux()
-    		data.update({'LS': light_lux})
-	except Exception, e:
-		pass
+    try:
+        light_sensor = TSL2561()
+        light_lux = light_sensor.readLux()
+        data.update({'LS': light_lux})
+    except Exception, e:
+        pass
 
     # import ipdb; ipdb.set_trace()
     for x in data:
         post(x, data[x])
-	print x
-	print data[x]
+        print x
+        print data[x]
 
 
 if __name__ == '__main__':
